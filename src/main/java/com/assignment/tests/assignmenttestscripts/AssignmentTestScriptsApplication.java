664fdd93b8f53cc3f9d4904050697776bf38db9d
fahad <https://fahad_tplmaps@bitbucket.org>
2020-02-13 21:56:53 +0500

    test scripts

---
+++ src/main/java/com/assignment/tests/assignmenttestscripts/AssignmentTestScriptsApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.tests.assignmenttestscripts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentTestScriptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentTestScriptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/tests/assignmenttestscripts/AssignmentTestScriptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.tests.assignmenttestscripts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentTestScriptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
